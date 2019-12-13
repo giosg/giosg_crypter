81962a43667a1fd99576a470370d3970e85e5b99
Joonas Kylliäinen <joonas@giosg.com>
2019-12-13 15:25:39 +0200

    added some input/output options and updated README

--- README.md
+++ README.md
@@ -8,11 +8,45 @@
 
 `pip install git+https://github.com/giosg/giosg_crypter.git#egg=giosg-crypter==<version number>`
 
+Where `<version number>` is the release version that user wants to install.
+
 ## Usage
 
 ### Command line
 
-`decrypt path/to/private/key/file path/to/chatsession/json/file`
+#### File inputs
+
+Provide paths to the files that contain chat_session and messages in file format.
+Flag the messages with `-m` or `--messages`.
+
+`decrypt path/to/private/key/file path/to/chatsession/json/file -m path/to/messages/json/file`
+
+#### Url inputs
+
+By providing a url that starts with `https`, the chat session and the messages can be fetched online.
+The API Token needs to be given with  `-t` or `--token` flag.
+In this case, url to the messages is not required.
+
+`decrypt path/to/private/key/file https://service.giosg.com/api/v5/path/to/chats/<chat_session_id> -t qef1h3iu2hfiu2h4ri2u3hri2u3hr`
+
+#### Choosing output
+
+By default, output is written in a file with default name `decrypted_messages.json`.
+The filename can be changed using `-f` or `--filename` flag:
+
+`decrypt path/to/private/key/file path/to/chatsession/json/file pat/to/messages/json/file -f new_filename.json`
+
+The output can also be changed to stdout with `-o` or `--output` flag:
+
+`decrypt path/to/private/key/file path/to/chatsession/json/file pat/to/messages/json/file -o stdout`
+
+#### Version
+
+`decrypt --version` or `decrypt -v` 
+
+#### Help
+
+`decrypt --help` or `decrypt -h` 
 
 ## As library
 
@@ -21,5 +55,5 @@ from giosg.crypter import AESKey, asymmetric_decrypt, symmetric_decrypt
 
 aes_key = asymmetric_decrypt(private_key, encrypted_key_from_chat_session)
 key = AESKey.from_json(aes_key)
-symmetric_decrypt(key, data["field_to_decrypt"])
+symmetric_decrypt(key, message_field_to_be_decrypted])
 ```
--- src/giosg/crypter/cli.py
+++ src/giosg/crypter/cli.py
@@ -1,6 +1,11 @@
 import subprocess
 import json
 import argparse
+import pprint
+
+import requests
+
+from ._version import get_version
 from . import AESKey, asymmetric_decrypt, symmetric_decrypt
 
 
@@ -34,36 +39,139 @@ class DecryptChatSession(object):
 
 class CommandLine(object):
     def __init__(self):
-        self.parser = argparse.ArgumentParser(usage='decrypt key chat messages',
+        self.parser = argparse.ArgumentParser(usage='decrypt key chat [-m --messages]',
                                               description='Decrypt Giosg chat.')
-        self.parser.add_argument('private_key', metavar='key', type=str,
-                                 help='path to the private key file')
-        self.parser.add_argument('chat_session', metavar='chat', type=str,
-                                 help='path to chat session json file')
-        self.parser.add_argument('messages', metavar='messages', type=str,
-                                 help='path to chat sessions messages json file')
+        self.parser.add_argument(
+            'private_key',
+            metavar='key',
+            type=str,
+            help='path to the private key file'
+        )
+        self.parser.add_argument(
+            'chat_session',
+            metavar='chat',
+            type=str,
+            help='path to chat session json file or url of the resource'
+        )
+        self.parser.add_argument(
+            '-m',
+            '--messages',
+            type=str,
+            help='path to chat sessions messages json file. Required if chat_session is file path'
+        )
+        self.parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version="%(prog)s " + get_version()
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output",
+            choices=['file', 'stdout'],
+            default='file',
+            help='How the results are returned: file or stdout. Defaults to file.'
+        )
+        self.parser.add_argument(
+            "-f",
+            "--filename",
+            type=str,
+            default='decrypted_messages.json',
+            help='Name of the file, where output is written. Defaults to decrypted_messages.json'
+        )
+        self.parser.add_argument(
+            "-t",
+            "--token",
+            help='If chat_session is a url resource, token for accessing giosg resources is required.'
+        )
 
     def arguments(self):
         return self.parser.parse_args()
 
 
-class Client(object):
+class Input(object):
+    def __init__(self, args):
+        if args.chat_session[:4] == 'http':
+            self._reader = UrlInputReader(args)
+        else:
+            self._reader = FileInputReader(args)
 
+    def reader(self):
+        return self._reader
+
+
+class InputReader(object):
     def __init__(self, args):
         self.private_key = args.private_key
         self.chat_session = args.chat_session
+
+    def read_private_key(self):
+        return subprocess.check_output(['openssl', 'rsa', '-in', self.private_key])
+
+
+class FileInputReader(InputReader):
+    def __init__(self, args):
+        super().__init__(args)
         self.messages = args.messages
 
-    def run(self):
-        private_key = subprocess.check_output(['openssl', 'rsa', '-in', self.private_key])
+    def read_chat_session(self):
         with open(self.chat_session, 'r') as f:
-            chat_session = json.loads(f.read())
+            return json.loads(f.read())
 
+    def read_messages(self):
         with open(self.messages, 'r') as f:
-            messages = json.loads(f.read())
+            return json.loads(f.read())
 
-        print(DecryptChatSession(private_key).decrypt(chat_session, messages))
+
+class UrlInputReader(InputReader):
+    def __init__(self, args):
+        super().__init__(args)
+        self.token = args.token
+
+    def read_chat_session(self):
+        return self._get_response(self.chat_session)
+
+    def read_messages(self):
+        return self._get_response(self.chat_session + "/messages")["results"]
+
+    def _get_response(self, url):
+        print("Fetching:", url)
+        response = requests.get(url, headers={"Authorization": "Token %s" % self.token})
+        response.raise_for_status()
+        return json.loads(response.content)
+
+
+class OutputWriter(object):
+
+    def __init__(self, args):
+        self.output_type = args.output
+        self.filename = args.filename
+
+    def write_output(self, output):
+        if self.output_type == 'file':
+            self._write_file(output)
+        else:
+            pprint.pprint(output)
+
+    def _write_file(self, output):
+        with open(self.filename, 'w') as f:
+            f.write(json.dumps(output, indent=4))
+
+
+class Client(object):
+
+    def __init__(self, input_reader, output_writer):
+        self.output_writer = output_writer
+        self.private_key = input_reader.read_private_key()
+        self.chat_session = input_reader.read_chat_session()
+        self.messages = input_reader.read_messages()
+
+    def run(self):
+        self.output_writer.write_output(DecryptChatSession(self.private_key).decrypt(self.chat_session, self.messages))
 
 
 def run():
-    Client(CommandLine().arguments()).run()
+    cmd_line_args = CommandLine().arguments()
+    inputs = Input(cmd_line_args).reader()
+    outputs = OutputWriter(cmd_line_args)
+    Client(inputs, outputs).run()
